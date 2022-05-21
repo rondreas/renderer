99c3068d50a40a5af892c1810c3aa38e2816cb21
Andreas Rånman <rondreas@users.noreply.github.com>
2022-05-21 23:23:33 +0200

    3. the vec3 class
    Added and implemented "most" traits ... I think ... and also added tests that we can run using "cargo test"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,84 @@
+// https://raytracing.github.io/books/RayTracingInOneWeekend.html
+
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::prelude::*; // TODO: Figure out which actual imports we use from this,
+
+use std::ops::{Neg, AddAssign, MulAssign, DivAssign};
+
+// Seems we can 'derive' and get some traits for free,
+// Debug here is used for printing the Vec3 in formatting
+// and PartialEq for when we make asserts_eq in tests.
+#[derive(Debug, PartialEq)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+// Implement operator traits,
+impl Neg for Vec3 {
+    type Output = Self; // TODO: figure out this standard,
+
+    fn neg(self) -> Self {
+        return Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        };
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        };
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        };
+    }
+}
+
+// Isn't this dot() ?
+trait LengthSquared {
+    fn length_squared(&self) -> f32;
+}
+
+impl LengthSquared for Vec3 {
+    fn length_squared(&self) -> f32 {
+        return self.x*self.x + self.y*self.y + self.z*self.z;
+    }
+}
+
+// Can't find how to extend .len() so doing this,
+trait Length {
+    fn length(&self) -> f32;
+}
+
+impl Length for Vec3 {
+    fn length(&self) -> f32 {
+        return self.length_squared().sqrt();
+    }
+}
 
 fn main() -> std::io::Result<()> {
     // File::create will return a Result, if it raises an error, the error
@@ -43,9 +122,50 @@ fn main() -> std::io::Result<()> {
     }
 
     // Write contents to file,
-    write!(file, "{}", contents);
+    write!(file, "{}", contents)?;
 
     eprint!("\nRender Finished\n");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vec3_neg() {
+        let a = Vec3{x: 1.0, y: 0.0, z: -3.0};
+        assert_eq!(-a, Vec3{x: -1.0, y: -0.0, z: 3.0});
+    }
+
+    #[test]
+    fn test_vec3_add_assign() {
+        let mut a = Vec3{x:1.0, y:2.0, z:3.0};
+        let b = Vec3{x:1.0, y:1.0, z:1.0};
+
+        a += b;
+
+        assert_eq!(a,Vec3{x: 2.0, y: 3.0, z: 4.0});
+    }
+
+    #[test]
+    fn test_vec3_mul_assign() {
+        let mut a = Vec3{x: 1.0, y: 2.0, z: 3.0};
+        let b = Vec3{x: 1.0, y: 2.0, z: 0.5};
+
+        a *= b;
+
+        assert_eq!(a, Vec3{x: 1.0, y: 4.0, z: 1.5});
+    }
+
+    #[test]
+    fn test_vec3_div_assign() {
+        let mut a = Vec3{x: 1.0, y: 2.0, z: 3.0};
+        let b = Vec3{x: 2.0, y: 3.0, z: 0.8};
+
+        a /= b;
+
+        assert_eq!(a,Vec3{x: 0.5, y: 0.6666667, z: 3.75});
+    }
 }
